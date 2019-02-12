3a87b8d8d17c4c19527d6478827caa1cedfde092
dyurein <epcijer@gmail.com>
2019-02-12 19:48:45 +0400

    test task for checking

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,10 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import JsSkill from '../blocks/js-skill/js-skill'
+
 
 $(() => {
 	svg4everybody();
+	const jsskill = new JsSkill();
+	jsskill.init();
 });
